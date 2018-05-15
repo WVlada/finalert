32255764d3266ca467c83b29a19559de60344135
Vladimir Bukvić <vladimir.bukvic@live.com>
2018-05-15 15:17:19 +0000

    prosli nije uspeo

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -3,6 +3,6 @@ class ContactController < ApplicationController
     
 
 	def create
-		NotificationsMailer.new_message(@message).deliver
+		#NotificationsMailer.new_message(@message).deliver
 	end
 end
